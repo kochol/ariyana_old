f217f545b68faa44eaae003bfb8580471bb3afcf
kochol <cyrusgp@gmail.com>
2018-07-10 12:48:57 +0430

    Create genie script files

---
+++ scripts/ariengine.lua
@@ -0,0 +1,18 @@
+project ("ariengine")
+	kind "SharedLib"
+
+	includedirs {
+		path.join(BGFX_DIR, "include"),
+		path.join(BX_DIR,   "include"),
+		path.join(BIMG_DIR, "include"),
+	}
+
+	links {
+		"bx",
+		"bgfx"
+	}
+
+	files {
+		"../include/ari/**.hpp",
+		"../src/engine/**.cpp",
+	}
