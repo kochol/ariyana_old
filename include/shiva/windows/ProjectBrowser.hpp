5cd4dcfa021824fb32bae8dbdb116f4b90bbc9a2
kochol <cyrusgp@gmail.com>
2018-10-25 18:14:19 +0330

    A test GUI component added to editor.

--- include/shiva/windows/ProjectBrowser.hpp
+++ include/shiva/windows/ProjectBrowser.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "shiva/shivadef.hpp"
 #include "ari/en/Entity.hpp"
+#include "ari/en/gui/Gui.hpp"
 
 namespace ari
 {
@@ -11,7 +12,17 @@ namespace ari
 
 namespace shiva
 {
-	
+	/*!
+	 * Custom GUI component to show a project in the list.
+	 */
+	class ProjectGui: public ari::Gui
+	{
+	public:
+		bool BeginRender() override;
+		void EndRender() override;
+
+	}; // ProjectGui
+
 	class SHIVA_API ProjectBrowser
 	{
 	public:
--- src/editor/windows/ProjectBrowser.cpp
+++ src/editor/windows/ProjectBrowser.cpp
@@ -23,6 +23,44 @@ namespace shiva
 		m_pWindow->Size.y = 600;
 		m_pWindow->Flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize;
 		m_Entity.AddChild(m_pWindow);
+		ProjectGui* pPG = new ProjectGui();
+		m_pWindow->AddChild(pPG);
+	}
+
+	bool ProjectGui::BeginRender()
+	{
+		ImGui::BeginGroup();
+		{
+			ImGui::BeginGroup();
+			ImGui::Button("AAA");
+			ImGui::SameLine();
+			ImGui::Button("BBB");
+			ImGui::SameLine();
+			ImGui::BeginGroup();
+			ImGui::Button("CCC");
+			ImGui::Button("DDD");
+			ImGui::EndGroup();
+			ImGui::SameLine();
+			ImGui::Button("EEE");
+			ImGui::EndGroup();
+			if (ImGui::IsItemHovered())
+				ImGui::SetTooltip("First group hovered");
+		}
+		// Capture the group size and create widgets using the same size
+		ImVec2 size = ImGui::GetItemRectSize();
+		const float values[5] = { 0.5f, 0.20f, 0.80f, 0.60f, 0.25f };
+		ImGui::PlotHistogram("##values", values, IM_ARRAYSIZE(values), 0, NULL, 0.0f, 1.0f, size);
+
+		ImGui::Button("ACTION", ImVec2((size.x - ImGui::GetStyle().ItemSpacing.x)*0.5f, size.y));
+		ImGui::SameLine();
+		ImGui::Button("REACTION", ImVec2((size.x - ImGui::GetStyle().ItemSpacing.x)*0.5f, size.y));
+		ImGui::EndGroup();
+
+		return false;
+	}
+
+	void ProjectGui::EndRender()
+	{
 	}
 
 } // shiva
