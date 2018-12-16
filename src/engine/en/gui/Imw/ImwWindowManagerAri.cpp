10f4045af59bc1d25749cc3c54f4bd0bb24a950e
kochol <cyrusgp@gmail.com>
2018-12-16 11:55:52 +0100

    Add window manager code.

--- src/engine/en/gui/Imw/ImwWindowManagerAri.cpp
+++ src/engine/en/gui/Imw/ImwWindowManagerAri.cpp
@@ -1,5 +1,50 @@
+#include "ImwWindowManagerAri.hpp"
+#include <bx/platform.h>
+#include "bgfx/bgfx.h"
+#include "ImwPlatformWindowAri.hpp"
+
+#if BX_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif
 
 namespace ari
 {
-	
+	bool ImwWindowManagerAri::CanCreateMultipleWindow()
+	{
+		const bgfx::Caps* caps = bgfx::getCaps();
+		return 0 != (caps->supported & BGFX_CAPS_SWAP_CHAIN);
+	}
+
+	ImWindow::ImwPlatformWindow* ImwWindowManagerAri::CreatePlatformWindow(ImWindow::EPlatformWindowType eType,
+		ImWindow::ImwPlatformWindow* pParent)
+	{
+		ImwPlatformWindowAri* pWindow = new ImwPlatformWindowAri(eType, CanCreateMultipleWindow());
+		if (pWindow->Init(pParent))
+		{
+			return pWindow;
+		}
+		delete pWindow;
+		return nullptr;
+	}
+
+	ImVec2 ImwWindowManagerAri::GetCursorPos()
+	{
+#if BX_PLATFORM_WINDOWS
+		POINT pt = { 0, 0 };
+		::GetCursorPos(&pt);
+		return { float(pt.x), float(pt.y) };
+#endif
+
+		return { 0.0f, 0.0f };
+	}
+
+	bool ImwWindowManagerAri::IsLeftClickDown()
+	{
+#if BX_PLATFORM_WINDOWS
+		return GetAsyncKeyState(VK_LBUTTON) & 0x8000;
+#endif
+
+		return false;
+	}
+
 } // ari
--- src/engine/en/gui/Imw/ImwWindowManagerAri.hpp
+++ src/engine/en/gui/Imw/ImwWindowManagerAri.hpp
@@ -1,6 +1,19 @@
 #pragma once
+#include "ImwWindowManager.h"
 
 namespace ari
 {
+	class ImwWindowManagerAri: public ImWindow::ImwWindowManager
+	{
+	protected:
+		bool CanCreateMultipleWindow() override;
+
+		ImWindow::ImwPlatformWindow* CreatePlatformWindow(ImWindow::EPlatformWindowType eType,
+			ImWindow::ImwPlatformWindow* pParent) override;
+
+		ImVec2 GetCursorPos() override;
+
+		bool IsLeftClickDown() override;
+	};
 
 } // ari
